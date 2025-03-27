b8956f3cdbb90e79d550a785dd3144a97c47a04c
praba <pdh@adorsys.com>
2025-03-27 10:33:34 +0000

    test the terraform

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -1,3 +1,10 @@
 variable "private_network_id" {}
+
 variable "project_id" {}
+
 variable "region" {}
+
+variable "cluster_name" {
+  description = "Unique name for the Kubernetes cluster"
+  type        = string
+}
