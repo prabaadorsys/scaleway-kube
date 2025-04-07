356bcbf8e27e582b5dcfe5e4269ffaa0d2bfe198
prabaadorsys <51906523+prabaadorsys@users.noreply.github.com>
2025-04-07 11:02:05 +0100

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -26,6 +26,6 @@ resource "scaleway_k8s_pool" "pool" {
   name        = "worker-pool"
   cluster_id  = scaleway_k8s_cluster.kapsule_multi_az.id
   node_type   = "COPARM1-2C-8G"
-  size        = 2  # Set worker node count to 2
+  size        = 1  # Set worker node count to 2
   autohealing = true
 }
